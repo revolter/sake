c3a90daae19162409ab279afcdefba2d7f808828
Pedro Piñera <pepibumur@gmail.com>
2017-11-21 13:47:35 +0100

    Add library & executable products to the Pakcage.swift

--- Package.swift
+++ Package.swift
@@ -5,15 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "sake",
+    products: [
+        .library(name: "SakefileDescription", type: .dynamic, targets: ["SakefileDescription"]),
+        .executable(name: "sake", targets: ["sake"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "sake",
-            dependencies: []),
-    ]
+        .target(name: "SakefileDescription", dependencies: []),
+        .target(name: "sake", dependencies: ["SakefileDescription"])
+        // .testTarget(name: "DangerTests", dependencies: ["Danger"]),
+    ],
+    swiftLanguageVersions: [4]
 )
