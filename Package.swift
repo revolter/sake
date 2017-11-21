028e58034605f1cd3f2fc50fc3f1ace4646534b4
Pedro Piñera <pepibumur@gmail.com>
2017-11-21 14:06:01 +0100

    Define CLI interface

--- Package.swift
+++ Package.swift
@@ -10,11 +10,12 @@ let package = Package(
         .executable(name: "sake", targets: ["sake"])
     ],
     dependencies: [
+        .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0")
     ],
     targets: [
         .target(name: "SakefileDescription", dependencies: []),
-        .target(name: "sake", dependencies: ["SakefileDescription"])
-        // .testTarget(name: "DangerTests", dependencies: ["Danger"]),
+        .target(name: "sake", dependencies: ["SakefileDescription", "Commander"]),
+        .testTarget(name: "sakeTests", dependencies: ["sake"])
     ],
     swiftLanguageVersions: [4]
 )
--- Sources/sake/main.swift
+++ Sources/sake/main.swift
@@ -1,1 +1,20 @@
-print("Hello, world!")
+import Commander
+
+Group {
+    let initCommand = command {
+        print("Not implemented yet")
+    }
+    let taskCommand = command(Argument<String>("task", description: "the task to be executed")) { task in
+        print("Not implemented yet")
+    }
+    let generateXcodeProjCommand = command {
+        print("Not implemented yet")
+    }
+    let tasksCommand = command {
+        print("Not implemented yet")
+    }
+    $0.addCommand("init", "initializes a Sakefile in the current directory", initCommand)
+    $0.addCommand("task", "runs the task passed", taskCommand)
+    $0.addCommand("tasks", "lists all the available tasks", tasksCommand)
+    $0.addCommand("generate-xcodeproj", "generates an Xcode project to edit the Sakefile", generateXcodeProjCommand)
+}.run()
