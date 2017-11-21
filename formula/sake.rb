59cb7db1539fefef3957ba18ee0497a1924f18a5
Pedro Piñera <pepibumur@gmail.com>
2017-11-21 13:49:50 +0100

    Add Homebrew formula

---
+++ formula/sake.rb
@@ -0,0 +1,15 @@
+class Sake < Formula
+    desc "Automate tasks with Swift"
+    homepage "https://github.com/pepibumur/sake"
+    version "0.1.0"
+    url "https://github.com/pepibumur/sake/archive/#{version}.tar.gz"
+    sha256 "xxx"
+    head "https://github.com/pepibumur/sake.git"
+
+    depends_on :xcode
+
+    def install
+        system "make", "install", "PREFIX=#{prefix}"
+    end
+    
+end
