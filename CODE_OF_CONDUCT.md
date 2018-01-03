33632b38420a554c328fafcabb589c83a2a15f48
Pedro <pepibumur@gmail.com>
2018-01-03 01:05:21 +0100

    Update links in the Formula and remove extra lines from the CODE_OF_conduct

--- CODE_OF_CONDUCT.md
+++ CODE_OF_CONDUCT.md
@@ -1,2 +1,1 @@
-
 Check out the organization [CODE OF CONDUCT](https://github.com/xcodeswift/contributors/blob/master/CODE_OF_CONDUCT.md)
--- Formula/sake.rb
+++ Formula/sake.rb
@@ -1,10 +1,10 @@
 class Sake < Formula
   desc "Automate tasks with Swift"
-  homepage "https://github.com/pepibumur/sake"
+  homepage "https://github.com/xcodeswift/sake"
   version "0.2.0"
-  url "https://github.com/pepibumur/sake/archive/#{version}.tar.gz"
+  url "https://github.com/xcodeswift/sake/archive/#{version}.tar.gz"
   sha256 "cbcfd74278a49a7e40d4a0a01371a39f2574cbc88a5be1f153b204dacf74ae7f"
-  head "https://github.com/pepibumur/sake.git"
+  head "https://github.com/xcodeswift/sake.git"
 
   depends_on :xcode
 
