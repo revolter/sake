4d733be16eb576d2749f4b800e7ec711f6ead1d5
Pedro <pepibumur@gmail.com>
2018-01-10 10:49:44 +0100

    Version 0.4.0

--- Formula/sake.rb
+++ Formula/sake.rb
@@ -1,7 +1,7 @@
 class Sake < Formula
   desc "Automate tasks with Swift"
   homepage "https://github.com/xcodeswift/sake"
-  url "https://github.com/xcodeswift/sake/archive/0.3.0.tar.gz"
+  url "https://github.com/xcodeswift/sake/archive/0.4.0.tar.gz"
   sha256 "06eba536e46758f64191f650cf7df4b7d33bc6aea9e822ab2668885ed50a3b3b"
   head "https://github.com/xcodeswift/sake.git"
 
@@ -12,15 +12,15 @@ class Sake < Formula
     ohai "Building Sake"
     library_path_swift_content = [
       "import Foundation",
-      "var librariesPath: String? = \"#{lib}\"",
+      "var librariesPath: String? = \"#{include}\"",
     ].join("\n") + "\n"
     File.write("#{buildpath}/Sources/SakeKit/LibraryPath.swift", library_path_swift_content)
     system("swift build --disable-sandbox -c release")
     bin.install sake_path
     ohai "Installing libraries"
-    lib.install "#{buildpath}/.build/release/libSakefileDescription.dylib"
-    lib.install "#{buildpath}/.build/release/SakefileDescription.swiftdoc"
-    lib.install "#{buildpath}/.build/release/SakefileDescription.swiftmodule"
+    include.install "#{buildpath}/.build/release/libSakefileDescription.dylib"
+    include.install "#{buildpath}/.build/release/SakefileDescription.swiftdoc"
+    include.install "#{buildpath}/.build/release/SakefileDescription.swiftmodule"
   end
 
   test do
